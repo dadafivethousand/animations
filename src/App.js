6139efe528a9973a0c920cb07dd287f52dd93f4e
maxim <dada@gmail.com>
2025-02-05 10:32:23 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
 import './Stylesheets/App.css'
-import GodfatherSchedule from "./Used/GodfatherSchedule";
-import HulkamaniaSchedule from "./Components/HulkamaniaSchedule";
-import HospitalSignSchedule from "./Components/HospitalSignSchedule";
-import GameOfThronesSchedule from "./Used/GameOfThronesSchedule";
-import PokemonSchedule from "./Used/PokemonSchedule";
-import HospitalSignsSchedule from "./Components/HospitalSignSchedule";
-import LOTRSchedule from "./Components/LOTRSchedule";
-import StillDreSchedule from "./Components/StillDreSchedule";
-import EminemSchedule from "./Components/EminemSchedule";
-import MapleWolverineSchedule from "./Used/WolverineSchedule";
-import WolverineSchedule from "./Used/WolverineSchedule";
-import SpiderManSchedule from "./Components/SpiderManSchedule";
-import HulkSchedule from "./Components/HulkSchedule";
-import SupermanSchedule from "./Components/SupermanSchedule";
-import VenomSchedule from "./Components/VenomSchedule";
-import BlackPantherSchedule from "./Components/BlackPantherSchedule";
+ 
+import KhabibSchedule from "./Components/KhabibSchedule";
 function App() {
   return (
     <>
-    <VenomSchedule day="Wednesday" />
+    <KhabibSchedule day="Thursday" />
    </>
   );
 }
---
+++ src/Components/KhabibSchedule.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "../Stylesheets/IronManSchedule.css";
+import schedule from "../Schedule";
+
+function IronManSchedule({ day }) {
+  const [showSchedule, setShowSchedule] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSchedule(true);
+    }, 500);
+  }, []);
+
+  // Convert decimal hours to AM/PM format
+  const formatTime = (decimalTime) => {
+    const hour = Math.floor(decimalTime);
+    const minutes = Math.round((decimalTime - hour) * 60);
+    const hour12 = hour % 12 === 0 ? 12 : hour % 12;
+    const amPm = hour < 12 ? "AM" : "PM";
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hour12}:${paddedMinutes} ${amPm}`;
+  };
+
+  return (
+    <div className="ironman-container">
+      {/* Arc Reactor in Background */}
+      <div className="arc-reactor"></div>
+
+      <h1 className="ironman-title">{day}</h1>
+
+      <div className="ironman-classes">
+        {showSchedule &&
+          schedule[day]?.map((cls, idx) => (
+            <div
+              key={idx}
+              className="ironman-class"
+              style={{ animationDelay: `${idx * 0.3}s` }}
+            >
+              <div className="ironman-class-name">{cls.name}</div>
+              <div className="ironman-class-time">{formatTime(cls.start)}</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default IronManSchedule;
