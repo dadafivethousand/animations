25efd28d7a9f0ea5e568b2bb87128994b447988c
maxim <dada@gmail.com>
2025-02-06 21:05:57 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './Stylesheets/App.css'
 import SimpsonsSchedule from "./Components/SimpsonsSchedule";
 import CloudySky from "./Components/Cloud";
 import HulkSchedule from "./Components/HulkSchedule";
+import LOTRSchedule from "./Components/LOTRSchedule";
 
  
  function App() {
   return (
     <>
-    <HulkSchedule day="Sunday" />
+    <LOTRSchedule day="Sunday" />
    </>
   );
 }
