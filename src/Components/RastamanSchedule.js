c8a6d559ce41ae26c2db07b603c40aeec4f3498c
dadafivethousand <maxli5004@gmail.com>
2025-02-14 16:34:12 -0500

    hi

--- src/Components/RastamanSchedule.js
+++ src/Components/RastamanSchedule.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import schedule from "../Schedule";
 import "../Stylesheets/RastamanSchedule.css"; // Ensure correct import path
 
-function RastamanSchedule({ day, smokeDelay = 1500 }) {
+function RastamanSchedule({ day, smokeDelay = 1000 }) {
   const [showSchedule, setShowSchedule] = useState(false);
   const [startSmoke, setStartSmoke] = useState(false);
   const [visibleClasses, setVisibleClasses] = useState([]); // Track individual animations
