5cbcff80d84523405084503402c8239e29f704a9
maxim <dada@gmail.com>
2025-04-04 22:38:41 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React from "react";
 import './Stylesheets/App.css'
  
  
-import SamuraiSchedule from "./Components/SamuraiSchedule"
-import BondBarrel from "./Components/BondBarrel";
-import SpinWheel from "./Components/SpinWheel";
-import TiestoSchedule from "./Components/TiestoSchedule";
+ 
+ 
+import BiggieSchedule from "./Components/BiggieSchedule"
 import GothicSchedule from "./Components/GothicSchedule";
 import MichaelJacksonSchedule from "./Components/MichaelJacksonSchedule";
 import WalkingDeadSchedule from "./Components/WalkingDeadSchedule";
 import ZombieSchedule from "./Components/ZombieSchedule";
-import WarcraftSchedule from "./Components/WarcraftSchedule";
+ 
  
   
  function App() {
   return (
     <>
-    <WarcraftSchedule day="Friday"/>
+    <BiggieSchedule day="Sunday"/>
    </>
   );
 }
