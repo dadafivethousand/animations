f7d56f09065851edde658f14564a5302c84d2c37
maxim <dada@gmail.com>
2025-04-07 00:13:53 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import './Stylesheets/App.css'
 import ThorSchedule from "./Components/ThorSchedule";
 import MapleStudios from "./Components/MapleMarvel";
+import BuzzSchedule from "./Components/BuzzSchedule"
  
  
   
  function App() {
   return (
     <>
-    <ThorSchedule day="Friday"/>
+    <BuzzSchedule day="Tuesday"/>
    </>
   );
 }
