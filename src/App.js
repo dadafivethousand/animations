c51675841fcbd8a2e2a96d67b5aab08d0fb7cb68
dadafivethousand <maxli5004@gmail.com>
2025-05-04 10:24:30 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React from "react";
-import './Stylesheets/App.css'
+import './Stylesheets/App.css';
+
  
-import GameBoy from "./Components/Gameboy"
-import GameBoySchedule from "./Components/GameboySchedule"
-import CrazyFrogSchedule from "./Components/CrazyFrogSchedule"
-import JurassicSchedule from "./Components/JurassicSchedule";
-import OfficeSchedule from "./Components/OfficeSchedule"
-import TokyoSchedule from "./Components/TokyoSchedule";
-import StrangerSchedule from "./Components/StrangerSchedule";
-import CasinoSchedule from "./Components/CasinoSchedule";
-import LavaSchedule from "./Components/LavaSchedule";
 import FerrariSchedule from "./Components/FerrariSchedule";
-import TeslaSchedule from "./Components/TeslaSchedule";
-import DiorSchedule from "./Components/DiorSchedule";
-import XboxSchedule from "./Components/XboxSchedule";
-import NasaSchedule from "./Components/NasaSchedule";
-import AnonymousSchedule from "./Components/AnonymousSchedule";
-import WitcherSchedule from "./Components/WitcherSchedule";
-import SixNineSchedule from "./Components/SixNineSchedule";
-<<<<<<< HEAD
-import KanyeSchedule from "./Components/KanyeSchedule";
-import KillBillSchedule from "./Components/KillBillSchedule";
-=======
-import KillBillSchedule from "./Components/KillBillSchedule";
-import GigaChadSchedule from "./Components/GigaChadSchedule"
-import GladiatorSchedule from "./Components/GladiatorSchedule";
->>>>>>> 55eee1f72d40c8620bab47d864780de0e6d33879
-  
-   
- function App() {
+ 
+
+function App() {
   return (
     <>
-<<<<<<< HEAD
-    < FerrariSchedule day="Friday" />
-=======
-    < GladiatorSchedule day="Sunday" />
->>>>>>> 55eee1f72d40c8620bab47d864780de0e6d33879
-   </>
+      <FerrariSchedule day="Friday" />
+      {/* You can toggle others like this */}
+      {/* <GladiatorSchedule day="Sunday" /> */}
+    </>
   );
 }
 
 export default App;
-
