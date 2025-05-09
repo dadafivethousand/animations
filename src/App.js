bd57ef22434a3ca8ea013f1980aa4b31d7939448
maxim <dada@gmail.com>
2025-05-09 07:23:18 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import ZeldaSchedule from "./Components/ZeldaSchedule";
  import FlappySchedule from "./Components/FlappySchedule";
 import TekkenSchedule from "./Components/TekkenSchedule";
 import GucciSchedule from "./Components/GucciSchedule";
+import CaptainAmericaSchedule from "./Components/CaptainAmericaSchedule"
 
   
 
 function App() {
   return (
     <>
-      <GucciSchedule day="Wednesday" />
+      <CaptainAmericaSchedule day="Saturday" />
 
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
