c26a02880d7ad7f55b3f6f65ec6d2725f0601e78
dadafivethousand <maxli5004@gmail.com>
2025-05-30 18:32:04 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './Stylesheets/App.css';
  
 import VegasSchedule from "./Components/VegasSchedule"
 import NightclubSchedule from "./Components/NightClubSchedule";
+import JoeRoganSchedule from "./Components/JoeRoganSchedule"
+import AlienSchedule from "./Components/AlienSchedule"
   
   
 
 function App() {
   return (
     <>
-      < NightclubSchedule day="Friday" />
+      < AlienSchedule day="Saturday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
