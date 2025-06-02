b92a3415c007a55aeb60423fc364d66a35b5459c
maxim <dada@gmail.com>
2025-06-02 11:38:02 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './Stylesheets/App.css';
  import JoeRoganSchedule from"./Components/JoeRoganSchedule"
  
 import Poster from "./Components/Poster"
+import AvatarSchedule from "./Components/AvatarSchedule";
   
   
 
 function App() {
   return (
     <>
-      < JoeRoganSchedule day="Monday" />
+      < AvatarSchedule day="Tuesday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
