5c5f17d2e79875ab2648108176694ed694579629
dadafivethousand <maxli5004@gmail.com>
2025-06-03 07:54:12 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './Stylesheets/App.css';
  import JoeRoganSchedule from"./Components/JoeRoganSchedule"
  
 import Poster from "./Components/Poster"
-  
+import LuxuryJewelrySchedule from "./Components/LuxuryJewelrySchedule";
   
 
 function App() {
   return (
     <>
-      < JoeRoganSchedule day="Monday" />
+      < LuxuryJewelrySchedule day="Wednesday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
