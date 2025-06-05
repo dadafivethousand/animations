8ab173749e884086aec7e9aecc0d393574e7fe53
dadafivethousand <maxli5004@gmail.com>
2025-06-05 13:25:24 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import LuxuryJewelrySchedule from "./Components/LuxuryJewelrySchedule";
 import EspnSchedule from "./Components/EspnSchedule";
 import AvatarSchedule from "./Components/AvatarSchedule";
 import AITerminalSchedule from "./Components/AITerminalSchedule"
+import MapleJiuJitsuHeader from "./Components/MapleJiuJitsuHeader";
    
 
 function App() {
   return (
     <>
-       <LuxuryJewelrySchedule day="Friday" />
+       <Poster />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
