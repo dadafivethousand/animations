d02272bc309cebc6cb45ec0e7d41e483be12977b
maxim <dada@gmail.com>
2025-06-07 17:03:31 -0400

    hi

--- src/Components/LouisVuittonSchedule.js
+++ src/Components/LouisVuittonSchedule.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../Stylesheets/LouisVuittonSchedule.css";
 import schedule from "../Schedule";
 
-export default function LouisVuittonSchedule({ day, animationDelay = 1800, animationInterval = 400 }) {
+export default function LouisVuittonSchedule({ day, animationDelay = 1800, animationInterval = 600 }) {
   const [visibleArray, setVisibleArray] = useState([]);
 
   useEffect(() => {
