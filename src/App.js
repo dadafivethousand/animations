41cef99e88d532c38109c3c21f746bcbdcfadc11
maxim <dada@gmail.com>
2025-06-11 07:21:25 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import ArmyCamoSchedule from "./Components/ArmyCamoSchedule";
 import YoungJeezySchedule from "./Components/YoungJeezySchedule";
 import HockeySchedule from "./Components/HockeySchedule";
  import VegasSchedule from "./Components/VegasSchedule";
-import Carter3Schedule from "./Components/Carter3Schedule";
+import Carter3Schedule from "./Used/Carter3Schedule";
 import MatrixSchedule from "./Used/Lit/MatrixSchedule"
 import MarioSchedule from "./Used/Lit/MarioSchedule"
 
 function App() {
   return (
     <>
-       < MarioSchedule day="Tuesday" />
+       < ArmyCamoSchedule day="Wednesday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
