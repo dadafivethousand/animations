77cf028ea622733a80ad565c25ccf3f002ca5360
maxim <dada@gmail.com>
2025-06-11 09:47:26 -0400

    hi

--- src/Components/TravisBarkerSchedule.js
+++ src/Components/TravisBarkerSchedule.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../Stylesheets/TravisBarkerSchedule.css";
 import schedule from "../Schedule";
 
-export default function TravisBarkerSchedule({ day, animationDelay = 300, animationInterval = 150 }) {
+export default function TravisBarkerSchedule({ day, animationDelay = 1800, animationInterval = 150 }) {
   const [visibleArray, setVisibleArray] = useState([]);
 
   useEffect(() => {
