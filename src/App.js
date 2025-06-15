f77d720621c77883bb46876357fa0584784a23f7
maxim <dada@gmail.com>
2025-06-15 12:48:51 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./Stylesheets/App.css"
 import GatoradeSchedule from "./Components/GatoradeSchedule";
 import VenomSchedule from "./Used/VenomSchedule"
+import HockeySchedule from "./Components/HockeySchedule";
  
 
 
 function App() {
   return (
     <>
-       <VenomSchedule day="Tuesday" />
+       <HockeySchedule day="Wednesday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
--- src/Components/HockeySchedule.js
+++ src/Components/HockeySchedule.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../Stylesheets/HockeySchedule.css";
 import schedule from "../Schedule";
 
-export default function HockeySchedule({ day, animationDelay = 300, animationInterval = 150 }) {
+export default function HockeySchedule({ day, animationDelay = 1800, animationInterval = 150 }) {
   const [visibleArray, setVisibleArray] = useState([]);
 
   useEffect(() => {
