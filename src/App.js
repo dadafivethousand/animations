2da8952194f5aa77617959ce47f57bf5bcc7b595
maxim <dada@gmail.com>
2025-06-18 21:40:26 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Stylesheets/App.css"
  
-import RolexSchedule from "./Used/Lit/RolexSchedule";
-import GodfatherSchedule from "./Used/Lit/GodfatherSchedule";
-import DJKhaledSchedule from "./Components/DJKhaledSchedule";
-import HitEmUpSchedule from "./Components/HitEmUpSchedule";
+import WarcraftSchedule from "./Used/WarcraftSchedule";
 
 function App() {
   return (
     <>
-       <DJKhaledSchedule day="Thursday" />
+       <WarcraftSchedule day="Thursday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
--- src/Used/WarcraftSchedule.js
+++ src/Used/WarcraftSchedule.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import "../Stylesheets/WarcraftSchedule.css";
-import schedule from "../Schedule"; // Assuming schedule data exists
+import "./WarcraftSchedule.css";
+import schedule from "../RhSchedule"; // Assuming schedule data exists
 
 function WarcraftSchedule({ day, animationDelay = 1000, animationInterval = 500 }) {
   const [showSchedule, setShowSchedule] = useState(false);
