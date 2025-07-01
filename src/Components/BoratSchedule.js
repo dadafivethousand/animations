0cf37a5af58b92756b54a0d0b3995152612997bd
maxim <dada@gmail.com>
2025-07-01 19:18:31 -0400

    hi

--- src/Components/BoratSchedule.js
+++ src/Components/BoratSchedule.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../Stylesheets/BoratSchedule.css";
-import schedule from "../RhSchedule";
+import schedule from "../Schedule";
 
 export default function BoratSchedule({ day, animationDelay = 1000, animationInterval = 250 }) {
   const [visible, setVisible] = useState([]);
