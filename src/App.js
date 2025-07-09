7894e2d34472b182e309c81ff5cba2bd029a5fea
maxim <dada@gmail.com>
2025-07-08 22:14:16 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import CodeSchedule from "./Used/CodeSchedule"
 function App() {
   return (
     <>
-       <  ShmurdaSchedule day="Wednesday" />
+       <  CodeSchedule day="Wednesday" />
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
