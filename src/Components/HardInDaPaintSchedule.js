60a117a048d423deed11c58a388505a8713d5907
maxim <dada@gmail.com>
2025-07-17 15:55:54 -0400

    hi

--- src/Components/HardInDaPaintSchedule.js
+++ src/Components/HardInDaPaintSchedule.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "../Stylesheets/HardInDaPaintSchedule.css";
 import schedule from "../Schedule";
+import FadeIn from "../Utils/FadeIn"
 
-function HardInDaPaintSchedule({ day, delay = 0 }) {
+function HardInDaPaintSchedule({ day, delay = 2000 }) {
   const [visibleClasses, setVisibleClasses] = useState([]);
   const [displayDay, setDisplayDay] = useState("");
 
   useEffect(() => {
+    setTimeout(() => {
     let i = 0;
     const typeInterval = setInterval(() => {
       setDisplayDay(day.substring(0, i + 1));
       i++;
       if (i > day.length) clearInterval(typeInterval);
     }, 80);
+        
+    }, delay);
+
   }, [day]);
 
   useEffect(() => {
@@ -50,7 +55,9 @@ function HardInDaPaintSchedule({ day, delay = 0 }) {
           ) : null
         )}
       </div>
+     <FadeIn speed={5000} delay = {1000}>
       <img src={'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXMzOG9kMHFjY2ljMmhmc3pnbXgzY3U0YnMzd2JwaGdpcjZsZm1mYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZHFoy2YVdMsus/giphy.gif'} />
+    </FadeIn>
     </div>
   );
 }
