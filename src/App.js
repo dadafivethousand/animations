86a6b816f0d6d3feceeee029a11b7a78f17b79f1
maxim <dada@gmail.com>
2025-07-27 23:56:10 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import LittleEinsteinsSchedule from "./Components/LittleEinsteinsSchedule";
 import WuTangSchedule from "./Components/WuTangSchedule";
 import BingoSchedule from "./Components/BingoSchedule";
 import FbiSchedule from "./Used/FBISchedule"
+import FlinstoneSchedule from "./Used/FlintstoneSchedule"
 function App() {
   return (
     <>
-  <FbiSchedule day={"Monday"}/>
+  <FlinstoneSchedule day={"Monday"}/>
       {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
