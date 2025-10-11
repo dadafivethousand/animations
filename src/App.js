6a715de91c8e232c84a1d2133c5f59eba499b6d0
maxim <dada@gmail.com>
2025-10-11 17:53:08 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,84 +1,11 @@
 import React from "react";
 import "./Stylesheets/App.css"
-import LegoSchedule from "./Components/LegoSchedule";
-import TTCSchedule from "./Components/TTCSchedule";
-import GOSchedule from "./Components/GOSchedule";
-import CharlieSchedule from "./Used/CharlieSchedule";
-import AmazonSchedule from "./Components/AmazonSchedule";
-import SouthParkSchedule from "./Used/SouthParkSchedule"
-import ADCCSchedule from "./Used/ADCCSchedule";
-import WuTangSchedule from "./Used/WuTangSchedule";
-import Tekashi6ix9ineSchedule from "./Used/Tekashi6ix9ineSchedule";
-import PandaSchedule from "./Used/PandaSchedule"
-import FriendsSchedule from "./Components/FriendsSchedule";
-import NikeSchedule from "./Components/NikeSchedule";
-import LooneyTunesSchedule from "./Components/LooneyTunesSchedule";
-import ESPNSchedule from "./Used/EspnSchedule";
-import ChiefKeefSchedule from "./Components/ChiefKeefSchedule";
-import BugattiSchedule from "./Components/BugattiSchedule";
-import JohnCenaSchedule from "./Components/JohnCenaSchedule";
-import ZenSchedule from "./Components/ZenSchedule";
-import BingoSchedule from "./Used/BingoSchedule";
-import HospitalSignsSchedule from "./Used/HospitalSignSchedule";
-import BoxingSchedule from "./Used/BoxingSchedule";
-import DanaherSchedule from "./Used/DanaherSchedule"
-import PaintItBlack from "./Components/PaintItBlack";
-import SpaceSchedule from "./Used/SpaceSchedule"
-import GordonRamsaySchedule from "./Components/GordonRamsaySchedule";
- import CadillacSchedule from "./Used/CadillacSchedule";
-import EASportsSchedule from "./Components/EASportsSchedule";
-import PopeyeSchedule from "./Components/PopeyeSchedule";
-import GogginsSchedule from "./Components/GogginsSchedule";
- import CashMeSchedule from "./Components/CashMeSchedule";
-import LionKingSchedule from "./Components/LionKingSchedule";
-import LionKingSunriseOverlay from "./Components/LionKingSunriseOverlay";
-import BassProSchedule from "./Components/BassProSchedule"
-import MoneySchedule from "./Components/MoneySchedule";
-import BobBuilderSchedule from "./Components/BobBuilderSchedule";
-import CasinoSchedule from "./Used/CasinoSchedule";
-import BondSchedule from "./Components/BondSchedule";
-import PowerRangersSchedule from "./Components/PowerRangersSchedule";
-import ThanosSchedule from "./Components/ThanosSchedule";
-import TeslaSchedule from "./Components/TeslaSchedule";
-import UndertakerSchedule from "./Used/UndertakerSchedule";
-import NabihaTypewriter from "./Components/Nabiha";
-import JamrockSchedule from "./Components/JamrockSchedule";
-import WhatsAppSchedule from "./Components/WhatsAppSchedule";
-import LyftSchedule from "./Components/LyftSchedule";
-import CashAppSchedule from "./Components/CashAppSchedule";
-import AvengersSchedule from "./Components/AvengersSchedule";
-import YSLSchedule from "./Components/YSLSchedule";
-import FortniteSchedule from "./Used/FortniteSchedule";
-import GameOfThronesSchedule from "./Used/GameOfThronesSchedule";
-import RockSchedule from "./Used/RockSchedule";
-import ReggaetonSchedule from "./Used/ReggaetonSchedule";
-import DontApproachSchedule from "./Components/DontApproachSchedule";
-import GriseldaBlancoSchedule from "./Components/GriseldaBlancoSchedule";
-import MGKWildBoySchedule from "./Components/MGKWildBoySchedule";
-import CodeNinjas from "./Components/CodeNinjas";
-import CodeBenefitsMatrix from "./Components/CodeBenefitsMatrix";
-import MonsterSchedule from "./Used/Lit/MonsterSchedule";
-import SimpsonsSchedule from "./Used/Lit/SimpsonsSchedule";
-import PokemonSchedule from "./Used/Lit/PokemonSchedule";
-import PulpFictionSchedule from "./Components/PulpFictionSchedule";
-import NirvanaSchedule from "./Used/NirvanaSchedule";
-import BrickSchedule from "./Components/BrickSchedule";
-import StarWarsIntro from "./Components/StarWarsIntro";
- import MicrosoftSchedule from "./Components/MicrosoftSchedule";
-import TikTokSchedule from "./Components/TikTokSchedule";
-import BlacksmithSchedule from "./Components/BlacksmithSchedule";
-import HeyArnoldSchedule from "./Components/HeyArnoldSchedule";
-import DiscordSchedule from "./Components/DiscordSchedule";
-import NinjaSchedule from "./Components/NinjaSchedule";
-import RoosterSchedule from "./Components/RoosterSchedule";
-import WildNOutSchedule from "./Components/WildNOutSchedule";
-import StraightOuttaSchedule from "./Used/StraightOuttaSchedule";
-import SamuraiSchedule from "./Used/Lit/SamuraiSchedule"
-import LionKingSunrise from "./Components/LionKingSunriseOverlay";
+
+import PeakyBlindersSchedule from "./Components/PeakyBlindersSchedule";
   function App() {
   return (
     <>
-  <HeyArnoldSchedule day="Sunday" />
+  <PeakyBlindersSchedule day="Monday" />
        {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
