d40fd1dff83cd3deaf53fc762b0e8d1eceaa1523
maxim <dada@gmail.com>
2025-10-17 07:28:37 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import BlacksmithSchedule from "./Components/BlacksmithSchedule";
    function App() {
   return (
     <>
-  <JohnWickSchedule day="Friday" />
+  <ElmoSchedule day="Friday" />
        {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
