2842073931c433822c45db25da4fe6f1c1ef2a43
maxim <dada@gmail.com>
2025-10-16 21:11:45 -0400

    hi;

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import BlacksmithSchedule from "./Components/BlacksmithSchedule";
    function App() {
   return (
     <>
-  <MGMGrandSchedule day="friday" />
+  <MGMGrandSchedule day="Friday" />
        {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
