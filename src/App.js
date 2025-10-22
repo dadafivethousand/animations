36be307318add970515e9f80000f22ba0028ab4e
maxim <dada@gmail.com>
2025-10-22 09:30:59 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import MetaSchedule from "./Components/MetaSchedule";
    function App() {
   return (
     <>
-  <PlayStationSchedule day="Thursday" />
+  <StarcraftSchedule day="Thursday" />
        {/* You can toggle others like this */}
       {/* <GladiatorSchedule day="Sunday" /> */}
     </>
--- src/Components/StarcraftSchedule.js
+++ src/Components/StarcraftSchedule.js
@@ -1,7 +1,7 @@
 // StarcraftSchedule.jsx
 import React, { useEffect, useState } from "react";
 import "../Stylesheets/StarcraftSchedule.css";
-import schedule from "../Schedule";
+import schedule from "../RhSchedule";
 
 export default function StarcraftSchedule({
   day,
